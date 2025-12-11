bb57da952c3d292a5462591c67d69b10d401b289
3dapi <emarhayyim@hotmail.com>
2025-12-12 00:52:02 +0900

    CLanguage-Sample 수정2

--- ch09.myPointer/ch09_ptr07_swap.c
+++ ch09.myPointer/ch09_ptr07_swap.c
@@ -2,16 +2,16 @@
 
 void swap(int* a, int* b)
 {
-	int temp = *a;  // 임시 변수에 *a 값을 저장.
-	*a = *b;        // b가 가리키는 주소의 값을 a 주소가 가르키는 주소에 저장.
-	*b = temp;      // temp 값을 b 주소가 가르키는 주소에 저장.
+	int temp = *a;	// 임시 변수에 *a 값을 저장한다.
+	*a = *b;		// b가 가르키는 주소의 값을 a 주소가 가르키는 주소에 저장한다.
+	*b = temp;		// temp 값을 b 주소가 가르키는 주소에 저장한다.
 }
 
 void swap_ptr_int(int** a, int** b)
 {
-	int* temp = *a; // a가 가리키는 포인터(즉, 원본 포인터)의 값을 temp에 보관.
-	*a = *b;        // a가 가리키는 실제 포인터 값을 b가 가리키는 포인터 값으로 변경.
-	*b = temp;      // b가 가리키는 포인터 값을 temp(원래 a가 가리 컸던 포인터)로 변경.
+	int* temp = *a;	// a가 가리키는 포인터(즉, 원본 포인터)의 값을 temp에 보관한다.
+	*a = *b;		// a가 가리키는 실제 포인터 값을 b가 가리키는 포인터 값으로 변경한다.
+	*b = temp;		// b가 가리키는 포인터 값을 temp(원래 a가 가리켰던 포인터)로 변경한다.
 }
 
 void swap_ptr_generic(void** a, void** b)
---
+++ ch10.myDefininedType/ch11_struct01.c
@@ -0,0 +1,58 @@
+﻿#include <stdio.h>
+
+struct Character
+{
+	char name[32];
+	int hp;
+	int attack;
+	float positionX, positionY;
+}
+myHero = { 	"Hello world", 100, 100, 10.0f, 20.0f };
+
+
+int main(void)
+{
+	{
+		// 이미 있는 구조체에 별칭 부여.
+		typedef struct Position Position;
+		typedef struct Position* PtrPosition;
+	}
+	{
+		// 이름 없는 구조체.
+		typedef struct {
+			float x, y;
+		} Position, * PtrPosition;
+	}
+	{
+		// 같은 이름으로 선언
+		typedef struct Position {
+			float x, y;
+		} Position, * PtrPosition;
+	}
+
+	struct A {
+		char c;   // 1바이트
+		int  n;   // 4바이트
+	};
+	struct B {
+		int  n;   // 4바이트
+		char c;   // 1바이트
+	};
+
+	size_t sizeA = sizeof(struct A); // 8바이트 (패딩 3바이트 포함)
+	size_t sizeB = sizeof(struct B); // 8바이트 (패딩 3바이트 포함)
+
+	static cc;
+	int c ={100}, d= {299};
+	enum COLOR_RGBACC
+	{
+		COLOR_RECCD ,          // 0
+	};
+	cc = 199;
+	
+	int aa[] ={1,2,3};
+
+	int** pp = &aa;
+	
+	return 0;
+}
